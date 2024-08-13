f03665eca60729a8d81ff60137b8700645256ed1
MadhavaRaoKolla <madhavaraokolla2@gmail.com>
2024-08-13 23:03:18 +0530

    Link added to comeback to movies page

--- src/app/movies/movie/[movieid]/page.tsx
+++ src/app/movies/movie/[movieid]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import movieStyles from "../../Movie.module.scss";
 import Navbar from "@/app/components/Navbar/Navbar";
 
@@ -17,21 +18,26 @@ const Movie = async ({
   return (
     <div className={movieStyles.box}>
       <Navbar />
-      <div className={movieStyles.data}>
-        <div className={movieStyles.info}>
-          <h1>{data.title}</h1>
-          <p>{data.overview}</p>
-          <p>Runtime of movie: {data.runtime}m</p>
-          <p>Available in '{data.original_language.toUpperCase()}' language</p>
-          <p>IMDB Rating: {data.vote_average}</p>
-          <p>Realeased on: {data.release_date}</p>
+      <div className={movieStyles.content}>
+        <div className={movieStyles.data}>
+          <div className={movieStyles.info}>
+            <h1>{data.title}</h1>
+            <p>{data.overview}</p>
+            <p>Runtime of movie: {data.runtime}m</p>
+            <p>
+              Available in '{data.original_language.toUpperCase()}' language
+            </p>
+            <p>IMDB Rating: {data.vote_average}</p>
+            <p>Realeased on: {data.release_date}</p>
+          </div>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+              alt={data.title}
+            />
+          </div>
         </div>
-        <div>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-            alt={data.title}
-          />
-        </div>
+        <Link href={`/movies`}>Go back</Link>
       </div>
     </div>
   );
